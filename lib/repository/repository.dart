7ee8140d48a2385d221bd7201ee52859e9e4364e
mayor04 <mayorwa4@gmail.com>
2021-10-26 12:13:59 +0100

    renamed repo to api

--- lib/repository/repository.dart
+++
@@ -1,8 +0,0 @@
-export 'authentication_repo/authentication_repo.dart';
-export 'authentication_repo/i_authentication_repo.dart';
-
-export 'organization_repo/i_organization_repo.dart';
-export 'organization_repo/organization_repo.dart';
-
-export 'channels_repo/channels_repo.dart';
-export 'channels_repo/i_channels_repo.dart';
